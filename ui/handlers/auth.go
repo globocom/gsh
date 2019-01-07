c74d9ba8ed727e16c32f52d384c80200ddc257e5
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-01-07 21:00:30 -0200

    ui: [FIX] Save session after generate state

--- ui/handlers/auth.go
+++ ui/handlers/auth.go
@@ -11,11 +11,13 @@ import (
 // Auth is a method that provides authentication flow using OpenID Connect
 func (h AppHandler) Auth(c echo.Context) error {
 	sess, _ := session.Get("session", c)
-	sess.Save(c.Request(), c.Response())
 
 	// generate radom state
 	state := random.String(32)
 	sess.Values["state"] = state
+
+	// save session
+	sess.Save(c.Request(), c.Response())
 
 	return c.Redirect(http.StatusFound, h.oauth2.AuthCodeURL(state))
 }
