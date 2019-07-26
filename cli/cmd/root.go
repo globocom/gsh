7bd226774e2700b263ba837a1a32a6ea3709a7a1
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-07-26 17:50:02 -0300

    cli: [FIX] Remove -t option from global command

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -72,10 +72,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gsh/config.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directlßy.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
